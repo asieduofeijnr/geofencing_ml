1e1abfc50488781b9ca9329417c3849e2e580717
Lance Santerre <lksanterre@dons.usfca.edu>
2024-02-05 08:15:00 -0800

    Lance push 1

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-def cleaning(file_location):
-    df = pd.read_csv(file_location)
-    ## Everything related to cleaning
-    return df
+
+def cleaning(Path):
+    """
+    This Function takes in a path of the data
+    returns a dataframe with id, lat , lon, and timestamp
+    """
+    data = pd.read_csv(Path,header=None)
+    selected_columns = data.iloc[:, [0, 2, 3, 4]]
+    selected_columns.columns = ['id','lat','lon','timestamp']
+    return selected_columns
