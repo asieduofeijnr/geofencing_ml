c1dc5b7fc1202726a0669f57b704fbebff915759
asieduofeijnr@gmail.com <asieduofeijnr@gmail.com>
2024-02-05 15:27:35 -0800

    added get location function

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from geopy.geocoders import Nominatim
+
 
 def haversine(lon1, lat1, lon2, lat2):
     """
@@ -24,6 +26,7 @@ def haversine(lon1, lat1, lon2, lat2):
     r = 6371  # Radius of earth in kilometers
     return c * r
 
+
 def get_stop_groups(data_frame):
     """
     Identifies groups of stops in the given DataFrame based on location data and stop duration,
@@ -36,53 +39,56 @@ def get_stop_groups(data_frame):
     - DataFrame: A pandas DataFrame with stop groups, their duration, coordinates, percentage of total stop time,
         and the corresponding timestamp of when the stop started.
     """
-    
+
     # Threshold for considering the car stopped, in kilometers (e.g., 0.05 km)
     stop_threshold = 0.05
     # Initialize lists for latitude, longitude, percentage, and timestamp
     percent = []
     timestamp = []
     latvalues = []
-    longvalues = [] 
-
+    longvalues = []
 
     # Calculate previous longitude and latitude
     data_frame['prev_lon'] = data_frame['lon'].shift()
     data_frame['prev_lat'] = data_frame['lat'].shift()
 
     # Calculate the distance using the Haversine formula
-    data_frame['distance'] = data_frame.apply(lambda row: \
-        haversine(row['lon'], row['lat'], row['prev_lon'], row['prev_lat'])\
-        if pd.notnull(row['prev_lon']) and pd.notnull(row['prev_lat']) else 0, axis=1)
-
-
+    data_frame['distance'] = data_frame.apply(lambda row:
+                                              haversine(
+                                                  row['lon'], row['lat'], row['prev_lon'], row['prev_lat'])
+                                              if pd.notnull(row['prev_lon']) and pd.notnull(row['prev_lat']) else 0, axis=1)
 
     # Identify rows where the car is stopped
     data_frame['stopped'] = data_frame['distance'] <= stop_threshold
 
     # Group continuous stopped periods
-    data_frame['stopped_group'] = (data_frame['stopped'] != data_frame['stopped'].shift()).cumsum()
+    data_frame['stopped_group'] = (
+        data_frame['stopped'] != data_frame['stopped'].shift()).cumsum()
     data_frame_stopped_group = data_frame[data_frame['stopped']].groupby('stopped_group')\
         .agg(start_time=('timestamp', 'min'), end_time=('timestamp', 'max'))
-    
+
     # Convert timestamps to datetime objects
     stopped_groups_df = pd.DataFrame(data_frame_stopped_group)
-    stopped_groups_df['start_time'] = pd.to_datetime(stopped_groups_df['start_time'])
-    stopped_groups_df['end_time'] = pd.to_datetime(stopped_groups_df['end_time'])
-    stopped_groups_df['time_diff'] =  stopped_groups_df['end_time'] - stopped_groups_df['start_time']
-    sorted_df_stopped_group = stopped_groups_df.sort_values(by='time_diff',ascending=False)
+    stopped_groups_df['start_time'] = pd.to_datetime(
+        stopped_groups_df['start_time'])
+    stopped_groups_df['end_time'] = pd.to_datetime(
+        stopped_groups_df['end_time'])
+    stopped_groups_df['time_diff'] = stopped_groups_df['end_time'] - \
+        stopped_groups_df['start_time']
+    sorted_df_stopped_group = stopped_groups_df.sort_values(
+        by='time_diff', ascending=False)
 
     # Create a dictionary to store stopped groups with their duration
     list_stopped = {}
     for index, row in sorted_df_stopped_group.iterrows():
         time_diff = row['time_diff']
         list_stopped[index] = [time_diff]
-    
+
     # Associate each stop group with its details
     dict_stopps = {}
-    for _,row in data_frame.iterrows():
+    for _, row in data_frame.iterrows():
         if row['stopped_group'] not in dict_stopps:
-            dict_stopps[row['stopped_group']]= row
+            dict_stopps[row['stopped_group']] = row
 
     # Update list_stopped with stop details
     for key in dict_stopps.keys():
@@ -108,6 +114,23 @@ def get_stop_groups(data_frame):
     df_reset['timestamp'] = timestamp
 
     # Drop the now unnecessary column
-    df_with_stops = df_reset.drop(df_reset.columns[2],axis=1)
+    df_with_stops = df_reset.drop(df_reset.columns[2], axis=1)
 
     return df_with_stops
+
+
+def get_location(lon, lat):
+    """
+    Retrieves the address corresponding to the given longitude and latitude.
+
+    Parameters:
+    - lon (str): The longitude of the location as a string.
+    - lat (str): The latitude of the location as a string.
+
+    Returns:
+    dict: A dictionary containing the address components of the location.
+    """
+    geolocator = Nominatim(user_agent='geoapiExcises')
+    location = geolocator.reverse(lat + "," + lon)
+    address = location.raw['address']
+    return address
